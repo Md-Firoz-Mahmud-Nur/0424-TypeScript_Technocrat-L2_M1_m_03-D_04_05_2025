fd14aeb3c52f4b430916f4de7aca88e154eac9e1
firoz.nur <firoznur5@gmail.com>
2025-03-07 08:16:55 +0600

    Add : Object & Literal Types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,15 @@
+// Reference Type --> Object
+
+const user: {
+  readonly company: "CodesRaft"; // type --> literal types
+  firstName: string;
+  middleName?: string; // optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "CodesRaft",
+  firstName: "Firoz",
+  // middleName: "Mahmud",
+  lastName: "Nur",
+  isMarried: false,
+};
