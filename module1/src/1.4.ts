b373efd0b411ead4ed9f753c7f1591b263bc74f0
firoz.nur <firoznur5@gmail.com>
2025-03-07 08:05:21 +0600

    Add : Basic Data Types

---
+++ module1/src/1.4.ts
@@ -0,0 +1,30 @@
+// Basic data types
+
+// string
+let firstName: string = "Nur";
+// Number
+let roll: number = 5;
+// boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
+
+let d: number;
+d = 5;
+
+// array
+let friends: string[] = ["Rahim", "Karim"];
+friends.push("Nur");
+
+let eligibleRollList: number[] = [1, 2, 3, 4];
+eligibleRollList.push(5);
+
+// tuple --> array --> order --> type of values
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string, boolean] = [50, "Nur", true];
+
+ageName[0] = 67;
