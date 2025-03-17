c4f8c1e95f372e7dae734317e67bed2746a19eae
firoz.nur <firoznur5@gmail.com>
2025-03-17 08:55:16 +0600

    Add : Generic Type

---
+++ module02/src/2.03-Generic Type.ts
@@ -0,0 +1,41 @@
+{
+  //Generic Type
+
+  type GenericArray<t> = Array<t>;
+
+  // const rollNumbers: number[] = [3, 4, 5];
+  const rollNumbers: GenericArray<number> = [3, 4, 5];
+
+  // const mentors: string[] = ["Firoz", "Mahmud", "Nur"];
+  const mentors: GenericArray<string> = ["Firoz", "Mahmud", "Nur"];
+
+  // const boolArray: boolean[] = [true, false, true];
+  const boolArray: GenericArray<boolean> = [true, false, true];
+
+  const user: GenericArray<{
+    name: string;
+    age: number | string;
+  }> = [
+    {
+      name: "Nur",
+      age: 20,
+    },
+    {
+      name: "Firoz",
+      age: "20",
+    },
+  ];
+
+  const add = (x: number, y: number) => x + y;
+
+  add(2, 3);
+
+  type GenericTuple<x, y> = [x, y];
+
+  const manush: GenericTuple<string, string> = ["Firoz", "Nur"];
+
+  const UserWithID: GenericTuple<number, { name: string; email: string }> = [
+    1234,
+    { name: "Nur", email: "a@b.com" },
+  ];
+}
